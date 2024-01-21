d662e1284d8cda341c7741a499e5c6096e712ca1
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-01-21 23:34:41 +0200

    fix

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,10 @@
+import notFoundImg from "../../img/404.png";
+
 export const NotFound = () => {
   return (
     <>
       <h1>404 Not found!</h1>
+      <img src={notFoundImg} alt="Not found" />
     </>
   );
 };
