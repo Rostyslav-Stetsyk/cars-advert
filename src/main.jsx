a516dc7f85dfec68e231b217f3285fc1e7610781
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-01-23 16:41:48 +0200

    fix favicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "../node_modules/modern-normalize/modern-normalize.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/cars-advert">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
