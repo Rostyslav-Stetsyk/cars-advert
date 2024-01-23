67cdee9f18e4593510020af0ad6e4637fb54ef6c
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2024-01-23 16:16:59 +0200

    change router to fix 404

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "../node_modules/modern-normalize/modern-normalize.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/cars-advert">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
